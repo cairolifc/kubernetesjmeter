3122cc7a8e273aa42b4b22f8ba84a412bba2d71b
Fer multipass <famosca@everis.com>
2020-03-10 16:26:35 +0100

    change jmeter version and docker tags

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="qaseatcode/jmeter-base:5.2.1" -f Dockerfile-base .
+docker build --tag="qaseatcode/jmeter-master:5.2.1" -f Dockerfile-master .
+docker build --tag="qaseatcode/jmeter-slave:5.2.1" -f Dockerfile-slave .
+#docker build --tag="qaseatcode/jmeter-reporter" -f Dockerfile-reporter .
